5470247e12430eb6515e19f25e365c75b1b06850
Robert <zaks.robert@bk.ru>
2022-09-15 23:42:56 +0300

    first

---
+++ dz2/3p34.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int fib(int n)
+{
+    if(n <= 0){
+        return 0;
+    } else if (n == 1){
+        return 1;
+    } else {
+        return fib(n-1)+fib(n-2);
+    }
+}
+
+int main(void)
+{
+    int i, n;
+    i = scanf("%d", &n);
+    if((i != 1) && (n >= 0)){
+        printf("Inccorect input");
+    }
+    for(i = 0; i <= n; i++){
+        printf("%d fibbonachi number is %d\n", i, fib(i));
+    }
+    return 0;
+}
