a6d539b7291c3995428ab20a189b8a44c17ba4c8
Robert <zaks.robert@bk.ru>
2022-09-15 23:47:27 +0300

    Update 3p34.c

--- dz2/3p34.c
+++ dz2/3p34.c
@@ -2,9 +2,9 @@
 
 int fib(int n)
 {
-    if(n <= 0){
+    if(n <= -1){
         return 0;
-    } else if (n == 1){
+    } else if ((n == 0) || (n == 1)){
         return 1;
     } else {
         return fib(n-1)+fib(n-2);
