d1bc06f825e3d8e3ad48b2d3620ae6a4dde42721
Robert <zaks.robert@bk.ru>
2022-10-13 00:09:57 +0300

    6p28

--- dz5/6p28.c
+++ dz5/6p28.c
@@ -1,100 +1,83 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "6p28.h"
 
-typedef struct inode {
-    double data;        /*TODO: change data to *data */
-    struct inode *next;
-} inode;
-
-typedef inode *list;
-
-void addfirst(list *a, double data)
+void swap_fst_lst(list *pl)
 {
-    list b = (list)malloc(sizeof(inode));
-    b -> data = data;
-    b -> next = *a;
-    *a = b;
+    list last, l = *pl;
+    if((l == NULL) || (l -> next == NULL)){
+        return;
+    }
+    while(l -> next -> next != NULL){
+        l = l -> next;
+    }
+    last = l -> next;
+    last -> next = (*pl) -> next;
+    (*pl) -> next = NULL;
+    l -> next = *pl;
+    *pl = last;
 }
 
-void rmfirst(list *a)
+void rm_first_eq_h(list l, double *data)
 {
-    list b = *a;
-    if(b == NULL){
-        return;
-    }
-    *a = b -> next;
-    free(b);
-}
-
-void rmlist(list *a)
-{
-    if(*a == NULL){
-        return;
-    }
-    rmfirst(a);
-}
-
-void printlist(list a)
-{
-    if(a == NULL){
-        return;
-    }
-    for(; a != NULL; a = a -> next){
-        printf("%lf ", a -> data);
-    }
-    putchar('\n');
-}
-
-void firstendswap(list *a)
-{
-    list c, b = *a;
-    if((b == NULL) || (b -> next == NULL)){
-        return;
-    }
-    while(b -> next -> next != NULL){
-        b = b -> next;
-    }
-    c = b -> next;
-    c -> next = (*a) -> next;
-    (*a) -> next = NULL;
-    b -> next = *a;
-    *a = c;
-}
-/*removes from the list the first occurrence of the element e
-with the given value list without a header*/
-void rmfirsteqnosen(list *a, double e)
-{
-    if(*a == NULL){
-        return;
-    }
-    if((*a) -> next == NULL){
-        free(a);
-        a = NULL;
-        return;
-    }
-    while((*a) -> next != NULL){
-        if((*a) -> next -> data == e){
-            rmfirst(&((*a) -> next));
+    list buf;
+    for(; l -> next != NULL; l = l -> next){
+        if(l -> next -> data == data){
+            buf = l -> next;
+            l -> next = buf -> next;
+            free(buf);
             break;
         }
     }
 }
 
-int main(void)
+void rm_first_eq_no_h(list *pl, double *data)
 {
-    list a = NULL;
-    addfirst(&a, 5.6);
-    addfirst(&a, 7.0);
-    addfirst(&a, 8.0);
-    addfirst(&a, 9.0);
-    addfirst(&a, -1.0);
-    addfirst(&a, 0.0);
-    addfirst(&a, 7.0);
-    printlist(a);
-    firstendswap(&a);
-    printlist(a);
-    rmfirsteqnosen(&a, 0.0);
-    printlist(a);
-    rmlist(&a);
-    return 0;
+    list buf;
+    for(; (*pl) != NULL; pl = &((*pl) -> next)){
+        if((*pl) -> data == data){
+            buf = *pl;
+            *pl = buf -> next;
+            free(buf);
+            break;
+        }
+    }
+}
+
+void rm_all_eq_h(list l, double *data)
+{
+    list buf;
+    for(; l -> next != NULL; l = l -> next){
+        if(l -> next -> data == data){
+            buf = l;
+            l -> next = buf -> next;
+            free(buf);
+        }
+    }
+}
+
+void rm_all_eq_no_h(list *pl, double *data)
+{
+    list buf;
+    while((*pl) != NULL){
+        if((*pl) -> data == data){
+            buf = *pl;
+            *pl = (*pl) -> next;
+            free(buf);
+        } else {
+            pl = &((*pl) -> next);
+        }
+    }
+}
+
+void cp_all_eq(list l, double *data)
+{
+    list buf;
+    for(; l != NULL; l = l -> next){
+        if(l -> data == data){
+            buf = (list)malloc(sizeof(inode));
+            buf -> data = data;
+            buf -> next = l;
+            *l = *buf;
+            l = l -> next;
+        }
+    }
 }
